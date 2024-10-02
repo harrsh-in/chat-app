4c32813d47781cdd9565aaf58c41051ecb9019b8
Harrsh Patel <harrsh-in@users.noreply.github.com>
2024-10-02 10:47:23 +0530

    Cleanup

--- backend/src/controllers/ping.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from 'koa';
-
-const pingController = (ctx: Context) => {
-    ctx.body = {
-        message: 'pong',
-    };
-};
-
-export default pingController;
--- backend/src/controllers/test-error.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'koa';
-
-const testErrorController = (ctx: Context) => {
-    ctx.throw(400, 'Test error');
-};
-
-export default testErrorController;
